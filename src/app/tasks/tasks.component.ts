b7e491a718420b860af3479c971a966582ad7380
Rahul Kumar <r.kumar@perficient.com>
2025-08-11 21:39:42 +0530

    Progress on Easy Task App

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,16 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss',
 })
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required:true }) name!: string;
+  isAddingTask = false;
+
   tasks = [
     {
     id: 't1',
@@ -36,6 +39,7 @@ export class TasksComponent {
     dueDate: '2024-06-15',
   },
   ]
+task: any;
 
   get selectedUserTasks() {
     return this.tasks.filter(task => task.userId === this.userId);
@@ -43,5 +47,13 @@ export class TasksComponent {
 
   onCompleteTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
   }
 }
